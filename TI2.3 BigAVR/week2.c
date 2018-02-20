a833cbb90cba280a8090daacd7145134122852f3
CoenB95 <boelhouwers.coen@gmail.com>
2018-02-20 13:57:22 +0100

    Add documentation, remove unused code.

--- TI2.3 BigAVR/week2.c
+++ TI2.3 BigAVR/week2.c
@@ -41,11 +41,6 @@ ISR ( INT1_vect )
 		nextTrigger = 1;
 		stepTaillight();
 	}
-	//if(x > 7 )
-	//{
-		//x = 4;
-	//}
-	//PORTD |= (1<<x);
 }
 
 ISR ( INT2_vect )
@@ -55,31 +50,41 @@ ISR ( INT2_vect )
 		nextTrigger = 0;
 		stepTaillight();
 	}
-	//PORTD &= ~(1<<x);
-	//x++;
-	//if(x > 7)
-	//{
-		//x = 4;
-	//}	
 }
 
-
+/*
+  Runs a chaser-effect on the leds connected to PORT-E.
+  The chaser effect can be controlled by buttons on PORTD.1 and PORTD.2.
+  PORTD.1 triggers one step of the chase effect on it's rising edge, while
+  PORTD.2 triggers one step of the chase effect on it's falling edge.
+*/
 void Week2_asignment2()
 {
 	xx = hxx * 2;
 	x = xx - 1;
 	
+	//INT_1 is connected to PORTD.1.
+	//INT_2 is connected to PORTD.2.
+	
+	//Set all PORTD ports to act as inputs.
 	DDRD = 0x00;
+	
+	//Set all PORTE ports to act as inputs.
 	DDRE = 0xFF;
 	
+	//Set INT_1's mode to rising-edge: 0b----_11--
+	//Set INT_2's mode to rising-edge: 0b--10_----
 	EICRA |= 0x2C;
+	
+	//Enable INT_1 and INT_2: 0b----_-11-
 	EIMSK |= 0x06;
 	
+	//Enable global interrupts.
 	sei();
 	
 	while (1)
 	{
-		
+		//Do nothing, program flow is determined by button presses interrupts.
 	}
 }
 
@@ -89,6 +94,12 @@ void main()
 	
 }
 
+/** \brief Step chaser-effect
+
+    Executes a single step in a chaser-effect over the leds of PORTE.
+	The method uses a single integer value to keep hold of the state, counting from zero to double the amount of leds.
+	That is because on the first run all leds are turned on one by one, and after that they need to be turned off.
+ */
 void stepTaillight() {
 	x = (x + 1)%xx;
 	if (x < hxx)
